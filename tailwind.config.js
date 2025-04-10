221f39c471ef9dd1512f76bf199dfb05dc5e8be7
swamimalode07 <swamimalode2004@gmail.com>
2025-04-11 00:23:45 +0530

    basic code setup added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+      "./*.html",
+      "./components/**/*.html",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        },
+        colors: {
+          primary: '#1e40af',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
